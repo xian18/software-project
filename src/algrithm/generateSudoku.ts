df669aab3e96e18c3985d7a0e376189ba147049f
Nicolasios <1498913522@qq.com>
2020-11-18 13:17:32 +0800

    modify the Sudoku

--- src/algrithm/generateSudoku.ts
+++ src/algrithm/generateSudoku.ts
@@ -6,6 +6,8 @@ import { sudokuValue, Level } from '../types';
  * @param level 表示难度,取值0,1,2,3; 0-随机, 1-3难度递增
  *
  * @returns tuple [generate,answer] generate:元组的第一个元素,9x9 matrix   answer:元组的第二个元素, 9x9 matrix
+ * 
+ * 
  */
 
 export const generateSudoku = (level: Level): sudokuValue[][][] => {
