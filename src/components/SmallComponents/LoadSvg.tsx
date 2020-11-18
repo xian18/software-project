e0705a367ced297390b74a598ac5bf29b60a7beb
SaltedFish <1010594446@qq.com>
2020-11-18 23:58:19 +0800

    上次没提交上来什么鬼。

---
+++ src/components/SmallComponents/LoadSvg.tsx
@@ -0,0 +1,14 @@
+import React, { FC, memo, SVGProps } from 'react';
+import {loadIcon} from '../../consts/elements';
+export const LoadSvg: FC<{
+    SvgProp?: SVGProps<SVGSVGElement>;
+    GProp?: SVGProps<SVGGElement>;
+}> = memo(({ SvgProp = {}, GProp = {} }) => (
+    <svg viewBox='0 0 24 24' {...SvgProp}>
+        <g  {...GProp}>
+            {loadIcon}
+        </g>
+    </svg>
+));
+
+export default LoadSvg;
